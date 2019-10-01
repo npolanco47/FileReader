3401ab46fd49f8c4120a031ac18d45a40bcf2f39
npolanco47 <npolanco@mail.csuchico.edu>
2019-09-30 22:32:30 -0700

    A few changes

--- main.cpp
+++ main.cpp
@@ -12,24 +12,33 @@ int main()
 
   cout<<"What file do you want to open? ";
   getline(cin, fileName);
+ 
+  fileStream.open(fileName.c_str(),ios::in);//STEP 1: open the fileStream for input, using the fileName specified
   
-  //STEP 1: open the fileStream for input, using the fileName specified
-  
-  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+  if(fileStream.is_open() /*STEP 2: check to see if the fileStream huccessfully opened*/ )
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
-    
-    //STEP 3: repeat the following until the end-of-file (eof) has been reached...
+  
+   
+   while(!fileStream.eof())
+   {
+     getline(fileStream, line);
+     cout << line << endl;
+
+ 
+  }
+    fileStream.close();
+   }
+   else
+   {
+     cout << fileName << " could not be opened.\n";
+   }
+   //STEP 3: repeat the following until the end-of-file (eof) has been reached...
     // 3A: read a line from fileStream into the variable line
     // 3B: display the line, followed by an endline
   
-    //STEP 4: close the fileStream
+   //STEP 4: close the fileStream
   
-  }
-  else
-  {
-    cout<<fileName<<" could not be opened.\n";
-  }
 
   return 0;
 }
